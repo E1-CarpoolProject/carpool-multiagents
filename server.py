67ed4158ffff4ef111e31d189960b057492284cc
gusi08 <a01653126@itesm.mx>
2021-11-30 18:05:51 -0600

    Entrega final

--- server.py
+++ server.py
@@ -9,29 +9,57 @@ from environment import ENVIRONMENT
 app = Flask(__name__, static_url_path="")
 model = CarpoolModel(
     environment=ENVIRONMENT,
-    passenger_limit=1,
-    passenger_inst_limit=1,
+    passenger_limit=0,
+    passenger_inst_limit=10,
     passenger_delay=1,
-    car_limit=1,
-    car_inst_limit=1,
+    car_limit=100,
+    car_inst_limit=25,
     car_delay=2,
 )
 port = int(os.getenv("PORT", 8585))
 
 
-@app.route("/")
-def root():
+@app.route("/prueba1")
+def prueba_uno():
     global model
     model = CarpoolModel(
-        environment=ENVIRONMENT,
-        passenger_limit=1,
-        passenger_inst_limit=1,
-        passenger_delay=1,
-        car_limit=1,
-        car_inst_limit=1,
-        car_delay=2,
-    )
-
+    environment=ENVIRONMENT,
+    passenger_limit=0,
+    passenger_inst_limit=10,
+    passenger_delay=1,
+    car_limit=100,
+    car_inst_limit=25,
+    car_delay=2,
+)
+    return jsonify([{"message":"Prueba 1"}])
+    
+@app.route("/prueba2")
+def prueba_dos():
+    global model
+    model = CarpoolModel(
+    environment=ENVIRONMENT,
+    passenger_limit=10,
+    passenger_inst_limit=10,
+    passenger_delay=1,
+    car_limit=5,
+    car_inst_limit=2,
+    car_delay=2,
+)
+    return jsonify([{"message":"Prueba 2"}])
+    
+@app.route("/prueba3")
+def prueba_tres():
+    global model
+    model = CarpoolModel(
+    environment=ENVIRONMENT,
+    passenger_limit=85,
+    passenger_inst_limit=85,
+    passenger_delay=1,
+    car_limit=15,
+    car_inst_limit=5,
+    car_delay=2,
+)
+    return jsonify([{"message":"Prueba 3"}])
 
 @app.route("/new_cars", methods=["GET"])
 def new_cars():
@@ -43,7 +71,6 @@ def new_cars():
 @app.route("/traffic_lights", methods=["GET"])
 def traffic_lights():
     traffic_data = model.get_traffic_lights_data()
-    print(traffic_data)
     return json.dumps(traffic_data)
 
 
